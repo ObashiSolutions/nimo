a6927294dc63abe314c54d67f0ee5fec2d647f4b
CapstoneMaryland <116642751+twinventures@users.noreply.github.com>
2026-05-19 14:37:06 -0400

    auto

--- laravel/app/Http/Controllers/StaffApplicantController.php
+++ laravel/app/Http/Controllers/StaffApplicantController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Applicant;
+use App\Models\ApplicantTimeline;
+use Illuminate\Support\Facades\Auth;
 
 class StaffApplicantController extends Controller
 {
