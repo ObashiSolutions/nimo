91f3dc065d9f1d38b8e5b7c45a2f21c629982f2a
CapstoneMaryland <116642751+twinventures@users.noreply.github.com>
2026-05-20 12:01:06 -0400

    auto update

--- laravel/app/Models/Payment.php
+++ laravel/app/Models/Payment.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'applicant_id',
+        'provider',
+        'reference',
+        'amount',
+        'currency',
+        'status',
+        'authorization_url',
+        'provider_response',
+    ];
+
+    protected $casts = [
+        'provider_response' => 'array',
+    ];
+
+    public function applicant()
+    {
+        return $this->belongsTo(Applicant::class);
+    }
 }
